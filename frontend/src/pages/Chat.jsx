c3924616e3acd449ec8b9c93084aef2bc89c38eb
morsa <voxfoot@gmail.com>
2022-10-23 20:02:17 +0500

    add get channels & mesages

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,9 +1,20 @@
 import useAuth from '../hooks/index.jsx';
+import axios from 'axios';
+
+const getChannelsAndMessages = async (token) => {
+  const response = await axios.get('/api/v1/data', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+
+  console.log(response.data);
+}
 
 const Chat = () => {
   const auth = useAuth();
-
-  console.log(auth.userData);
+  const { token } = auth.userData;
+  getChannelsAndMessages(token);
 
   return (
     <div>
