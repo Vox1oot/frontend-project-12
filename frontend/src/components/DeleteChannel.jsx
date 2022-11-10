89fd23b0e4e2b3ec5375dc1f803125b39bb18249
morsa <voxfoot@gmail.com>
2022-11-10 21:15:50 +0500

    add delay unlock for delete channel

--- frontend/src/components/DeleteChannel.jsx
+++ frontend/src/components/DeleteChannel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Dropdown } from 'react-bootstrap';
@@ -6,6 +6,8 @@ import { Dropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 import { toastWarning } from '../toasts/index.js';
+
+import unlockElementWithDelay from '../utils/unlockElementWithDelay.js';
 
 export const DeleteChannel = ({ socket, id }) => {
   const { t } = useTranslation();
@@ -17,14 +19,21 @@ export const DeleteChannel = ({ socket, id }) => {
   const deleteChannel = () => {
     setSubmitting(true);
 
-    socket.emit('removeChannel', { id }, (response) => {
-      if (response.status === 'ok') {
+    socket.emit('removeChannel', { id }, ({ status }) => {
+      if (status) {
         toastWarning(t('toasts.delete'));
         setSubmitting(false);
         toggleModal();
       }
     });
   };
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const toggle = unlockElementWithDelay(setSubmitting, 3000);
+      toggle(false);
+    }
+  }, [isSubmitting])
 
   return (
     <>
@@ -38,7 +47,7 @@ export const DeleteChannel = ({ socket, id }) => {
         </Modal.Header>
         <Modal.Body>{t('questions.areYouSure')}</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={toggleModal}>
+          <Button variant="secondary" onClick={toggleModal} disabled={isSubmitting}>
             {t('buttons.cancel')}
           </Button>
           <Button variant="danger" onClick={deleteChannel} disabled={isSubmitting}>
