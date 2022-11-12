bd5a0a67ffd23ca98ffeff91296c127fa77fd47f
morsa <voxfoot@gmail.com>
2022-11-13 00:05:13 +0500

    fix: eslint [Channels]

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from "react";
-import { Button, ButtonGroup } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-
-import { changeChannel } from '../redux/slices/channelsSlice.js';
-import { DeleteChannel } from "./DeleteChannel.jsx";
-import { RenameChannel } from "./RenameChannel.jsx";
+import React, { useEffect } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Dropdown from 'react-bootstrap/Dropdown';
+import { changeChannel } from '../redux/slices/channelsSlice.js';
+import { DeleteChannel } from './DeleteChannel.jsx';
+import { RenameChannel } from './RenameChannel.jsx';
 
 const Channels = ({ socket }) => {
   const { channels, currentChannelId } = useSelector((state) => state.channels);
@@ -18,33 +17,36 @@ const Channels = ({ socket }) => {
 
   useEffect(() => {
     dispatch(changeChannel(currentChannelId));
-  }, [dispatch, currentChannelId]) // currentChannelId render twice 
+  }, [dispatch, currentChannelId]);
 
   return (
     channels.map((channel, index) => (
+      // eslint-disable-next-line react/no-array-index-key
       <li className="nav-item w-100" key={index}>
         <Dropdown className="w-100" as={ButtonGroup}>
-        <Button
-          className='br-0 btn-channel text-truncate text-start br-0"'
-          variant="light" 
-          id={channel.id}
-          active={currentChannelId == channel.id}
-          onClick={handleActiveChannel}
+          <Button
+            className='br-0 btn-channel text-truncate text-start br-0"'
+            variant="light"
+            id={channel.id}
+            active={currentChannelId === channel.id}
+            onClick={handleActiveChannel}
           >
-            <span className="me-1">#</span>{channel.name}
+            <span className="me-1">#</span>
+            {channel.name}
           </Button>
-        
-        {channel.removable &&
-        <Dropdown.Toggle className="br-0" split variant="light" id="dropdown-split-basic" >
+
+          {channel.removable
+        && (
+        <Dropdown.Toggle className="br-0" split variant="light" id="dropdown-split-basic">
           <span className="visually-hidden">Управление каналом</span>
         </Dropdown.Toggle>
-        }
-        
-        <Dropdown.Menu>
-          <DeleteChannel socket={socket} id={channel.id} />
-          <RenameChannel socket={socket} id={channel.id} />
-        </Dropdown.Menu>
-      </Dropdown>
+        )}
+
+          <Dropdown.Menu>
+            <DeleteChannel socket={socket} id={channel.id} />
+            <RenameChannel socket={socket} id={channel.id} />
+          </Dropdown.Menu>
+        </Dropdown>
       </li>
     ))
   );
