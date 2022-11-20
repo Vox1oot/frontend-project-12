2b85e78b7b9eee9b5177815c25368882be322639
morsa <voxfoot@gmail.com>
2022-11-20 12:11:59 +0500

    from test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles/style.css';
 import runApp from './init';
+/* ewfregreg */
 
 runApp();
