d804258c82051de2770f48074df4c680cf9bde97
morsa <voxfoot@gmail.com>
2022-11-20 13:14:06 +0500

    refactor: deleted useEffect from Channels.jsx

--- frontend/src/pages/chat/components/Channels.jsx
+++ frontend/src/pages/chat/components/Channels.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -14,10 +14,6 @@ const Channels = () => {
   const handleActiveChannel = (e) => {
     dispatch(changeChannel(Number(e.target.id)));
   };
-
-  useEffect(() => {
-    dispatch(changeChannel(currentChannelId));
-  }, [dispatch, currentChannelId]);
 
   return (
     channels.map((channel, index) => (
